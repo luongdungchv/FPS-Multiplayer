c10d11202a18a8c8026779e9ffaa42b2b94eaa65
flow29 <luongdungchv@gmail.com>
2024-10-26 19:47:17 +0700

    Merge Sever

--- Assets/_Game/Scripts/Client/CustomNetworkHandleClient.cs
+++ Assets/_Game/Scripts/Client/CustomNetworkHandleClient.cs
@@ -9,8 +9,10 @@ namespace Kigor.Networking
     {
 #if CLIENT_BUILD
         public UnityAction<int, FPSPlayerState> OnReconcilePacketReceived;
+        public UnityAction<byte, Vector3> OnPlayerShotPacketReceived;
         partial void Initialize(){
             this.udpHandleMap.Add(PacketType.FPS_RECONCILE_PACKET, HandleFPSReconciliation);
+            this.tcpHandleMap.Add(PacketType.FPS_PLAYER_SHOT, this.HandlePlayerShot);
         }
 
         private void HandleFPSReconciliation(byte[] msg){
@@ -23,6 +25,21 @@ namespace Kigor.Networking
             }
             
             this.OnReconcilePacketReceived?.Invoke(packet.tick, packet.playerState);
+        }
+
+        private void HandlePlayerShot(byte[] msg)
+        {
+            
+            var packet = new FPSPlayerShotPacket();
+            try{
+                packet.DecodeMessage(msg);
+            }
+            catch(System.Exception e){
+                Debug.LogError(e);
+            }
+            Debug.Log($"Player shot received: {packet.playerID}, {packet.hitPos}");
+
+            this.OnPlayerShotPacketReceived?.Invoke(packet.playerID, packet.hitPos);
         }
 #endif
     }
