e7876aeeb52201bc13de7e7e62ae8f6b5b363cf3
luongdungchv <luongdungchv@gmail.com>
2024-11-19 19:21:00 +0700

    temp

---
+++ Assets/CustomIK/RiggingConstraint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kigor.Rigging
+{
+    public abstract class RiggingConstraint : MonoBehaviour
+    {
+        public abstract void SolveIK();
+    }
+}
--- Assets/_Game/Scripts/Shared/PlayerAnimationController.cs
+++ Assets/_Game/Scripts/Shared/PlayerAnimationController.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Animations.Rigging;
 using UnityEngine.Playables;
+using Kigor.Rigging;
 
 public class PlayerAnimationController : MonoBehaviour
 {
     [SerializeField] private string[] animationStates;
-    [SerializeField] private MultiAimConstraint rightHandAim, chestAim;
+    [SerializeField] private List<RiggingConstraint> riggingConstraints;
     private Transform aimTargetPoint;
     private Animator animator;
 
@@ -22,57 +23,9 @@ public class PlayerAnimationController : MonoBehaviour
     {
         this.animator = this.GetComponent<Animator>();
         this.animator.speed = 0;
-
-        var aimTargetObj = new GameObject("AimTarget");
-        this.aimTargetPoint = aimTargetObj.transform;
-        
-        var weightedPoint = new WeightedTransform(this.aimTargetPoint, 1);
-        WeightedTransformArray sources = new WeightedTransformArray();
-        
-        sources.Add(weightedPoint);
-        this.rightHandAim.data.sourceObjects = sources;
-        this.chestAim.data.sourceObjects = sources;
-        this.rigBuilder.Build();
-
-        this.rigBuilder.graph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-        this.rigBuilder.enabled = false;
+        this.animator.enabled = true;
     }
 
-    private void OnEnable()
-    {
-        // this.rigBuilder.enabled = false;
-        // playableGraph = PlayableGraph.Create(gameObject.name + ".HumanAnim");
-        // playableGraph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-        //
-        // // Create main playable
-        // var mainPlayable = AnimatorControllerPlayable.Create(playableGraph, this.animator.runtimeAnimatorController);
-        // var mainOutput = AnimationPlayableOutput.Create(playableGraph, "MainOutput", this.animator);
-        // mainOutput.SetSourcePlayable(mainPlayable);
-        
-        // Debug.Log(rigBuilder.Build(playableGraph));
-        // this.animator.playableGraph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-        //this.rigBuilder.graph.SetTimeUpdateMode(DirectorUpdateMode.Manual);
-    }
-
-    private void LateUpdate()
-    {
-        //this.playableGraph.Evaluate(Time.deltaTime);
-        // this.animator.speed = 1;
-        // this.animator.Update(Time.deltaTime);
-        // this.animator.speed = 0;
-
-        var directionIndicator = this.GetComponent<PlayerAvatar>().DirectionIndicator;
-        this.aimTargetPoint.position = directionIndicator.transform.position + directionIndicator.forward * 1000;
-
-        this.rigBuilder.graph.Evaluate(Time.deltaTime);
-    }
-
-    private void Update()
-    {
-        this.animator.speed = 1;
-        this.animator.Update(Time.deltaTime);
-        this.animator.speed = 0;
-    }
 
     public void ChangeAnimationState(int stateIndex)
     {
@@ -91,7 +44,7 @@ public class PlayerAnimationController : MonoBehaviour
     {
         this.animator.speed = 1;
         this.animator.Update(deltaTime);
+        this.riggingConstraints.ForEach(x => x.SolveIK());
         this.animator.speed = 0;
-        
     }
 }
